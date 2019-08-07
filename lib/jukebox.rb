87a3edcd68507759036419c1838099487a6a4b5a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:50:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,18 +8,6 @@
  
 # puts say_hello(users_name)
 
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
-
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -34,10 +22,22 @@ def list(songs)
   end 
 end 
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def play(songs)
   puts "Please enter a song name or number:"
   response = gets.strip
-  songs.collect do |song|
+  songs.each.with_index(1) do |song, i|
     if response == song || response == rand(1..9)
       puts "Playing <song name>"
     else 
