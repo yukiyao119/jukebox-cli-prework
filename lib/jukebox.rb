0e163f8d00505a1ce17a22db788953113d8c1286
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 06:10:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,7 +39,7 @@ def play(songs)
   list(songs)
   puts "Please enter a song name or number:"
   response = gets.strip
-  # binding.pry
+  binding.pry
   songs.each_with_index do |song, i|
     # binding.pry 
     if number.include?(response.to_i)
